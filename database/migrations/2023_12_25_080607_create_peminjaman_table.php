8d7224026fc02cee37521b31f221dbb35068477b
Elsiyuliani21 <143062239+Elsiyuliani21@users.noreply.github.com>
2023-12-25 18:49:12 +0700

    firts commit

--- database/migrations/2023_12_25_080607_create_peminjaman_table.php
+++ database/migrations/2023_12_25_080607_create_peminjaman_table.php
@@ -11,12 +11,8 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('employees', function (Blueprint $table) {
+        Schema::create('peminjaman', function (Blueprint $table) {
             $table->id();
-            $table->string('nama');
-            $table->integer('nomor_induk');
-            $table->integer('no_telpon');
-            $table->text('alamat');
             $table->timestamps();
         });
     }
@@ -26,6 +22,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('employees');
+        Schema::dropIfExists('peminjaman');
     }
 };
---
+++ resources/views/datamahasiswa.blade.php
@@ -0,0 +1,57 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-nU14brUcp6StFntEOOEBvcJm4huWjB0OcIeQ3fltAfSmuZFrkAif0T+UtNGlKKQv" crossorigin="anonymous">
+
+    <title> perpustakaan!</title>
+  </head>
+  <body>
+    <h1>peminjam!</h1>
+    <table class="table">
+      <thead>
+        <tr>
+          <th scope="col">#</th>
+          <th scope="col">nama</th>
+          <th scope="col">nim</th>
+          <th scope="col">prodi</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <th scope="row">1</th>
+          <td>nur fitri amalia</td>
+          <td>20110268</td>
+          <td>IFSI S1</td>
+        </tr>
+        <tr>
+          <th scope="row">2</th>
+          <td>elsi yuliani</td>
+          <td>20112202</td>
+          <td>IFSI S1</td>
+        </tr>
+        <tr>
+          <th scope="row">3</th>
+          <td>indria dwiyanti</td>
+          <td>20344987</td>
+          <td>IFSI S1</td>
+        </tr>
+      </tbody>
+    </table>
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
+    -->
+  </body>
+</html>
